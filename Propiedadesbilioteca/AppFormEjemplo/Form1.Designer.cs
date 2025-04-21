41494bc47b26fa44a9892c46e24e88d0ce6563f2
isaacescobar915 <zaacdf@hotmail.com>
2025-04-21 08:52:59 -0500

    Add files via upload

---
+++ Propiedadesbilioteca/AppFormEjemplo/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AppFormEjemplo
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Location = new System.Drawing.Point(20, 20);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(100, 20);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(120, 20);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.Location = new System.Drawing.Point(20, 50);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(100, 20);
+            this.lblPrecio.TabIndex = 2;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(120, 50);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(120, 80);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(200, 30);
+            this.btnMostrar.TabIndex = 4;
+            this.btnMostrar.Text = "Mostrar Producto";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Location = new System.Drawing.Point(20, 120);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(350, 80);
+            this.lblResultado.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Gestión de Productos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ Propiedadesbilioteca/AppFormEjemplo/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BibliotecaEjemplo;
+
+namespace AppFormEjemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigurarInterfaz();
+        }
+
+        private void ConfigurarInterfaz()
+        {
+            // Configuración básica del formulario
+            this.Text = "Gestión de Productos";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.ClientSize = new Size(400, 250);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+
+            // Configuración de controles
+            lblNombre.Location = new Point(20, 20);
+            lblNombre.Text = "Nombre:";
+            lblNombre.AutoSize = true;
+
+            txtNombre.Location = new Point(120, 20);
+            txtNombre.Width = 200;
+
+            lblPrecio.Location = new Point(20, 50);
+            lblPrecio.Text = "Precio:";
+            lblPrecio.AutoSize = true;
+
+            txtPrecio.Location = new Point(120, 50);
+            txtPrecio.Width = 100;
+
+            btnMostrar.Location = new Point(120, 80);
+            btnMostrar.Text = "Mostrar Producto";
+            btnMostrar.Width = 200;
+            btnMostrar.Click += btnMostrar_Click;
+
+            lblResultado.Location = new Point(20, 120);
+            lblResultado.AutoSize = false;
+            lblResultado.Width = 350;
+            lblResultado.Height = 80;
+            lblResultado.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("Ingresa un nombre válido.", "Error",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!decimal.TryParse(txtPrecio.Text, out decimal precio) || precio <= 0)
+            {
+                MessageBox.Show("Ingresa un precio válido (mayor que 0).", "Error",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Producto producto = new Producto
+            {
+                Nombre = txtNombre.Text.Trim(),
+                Precio = precio
+            };
+
+            lblResultado.Text = producto.ObtenerDescripcion();
+        }
+
+        private void lblNombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
