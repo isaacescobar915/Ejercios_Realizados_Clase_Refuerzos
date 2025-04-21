d6cfc6779c257d4e1280f55cf5ebec8538e5f3a9
isaacescobar915 <zaacdf@hotmail.com>
2025-04-21 08:51:50 -0500

    Add files via upload

---
+++ ManejoExcepciones/ManejoExcepciones/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ManejoExcepciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(104, 176);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(95, 37);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 1;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(95, 82);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 2;
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(107, 124);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 3;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnDividir;
+    }
+}
+
---
+++ ManejoExcepciones/ManejoExcepciones/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManejoExcepciones
+{
+    public partial class Form1 : Form
+
+    {
+
+        public Form1()
+
+        {
+
+            InitializeComponent();
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+
+            ConfigurarInterfaz();
+        }
+
+        private void ConfigurarInterfaz()
+        {
+            // Configuración básica del formulario
+            this.Text = "Manejo de Excepciones";
+            this.ClientSize = new Size(400, 250);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+            // Label
+
+
+            lblResultado.Location = new Point(100, 160);
+            lblResultado.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            lblResultado.Size = new Size(300, 30);
+         
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+
+        {
+
+            try
+
+            {
+
+                int numero1 = int.Parse(txtNumero1.Text);
+
+                int numero2 = int.Parse(txtNumero2.Text);
+
+
+
+                int resultado = numero1 / numero2;
+
+
+
+                lblResultado.Text = $"Resultado: {resultado}";
+
+            }
+
+            catch (FormatException)
+
+            {
+
+                MessageBox.Show("Por favor, introduce números válidos.", "Error de Formato",
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+            catch (DivideByZeroException)
+
+            {
+
+                MessageBox.Show("No es posible dividir entre cero.", "División por cero",
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show($"Error inesperado: {ex.Message}", "Error",
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            finally
+
+            {
+
+                txtNumero1.Clear();
+
+                txtNumero2.Clear();
+
+                txtNumero1.Focus();
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
